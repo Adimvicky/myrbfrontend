812889aa50f8bf5e46ce51ac9ece73763efada4a
Adim <adimvicky@gmail.com>
2020-08-17 16:39:50 +0100

    Add build files

---
+++ build/precache-manifest.3fdaff56cb7e12aaa1fb4df12ceadae8.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41ef2e9a38212b5ff398a8045e3069eb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f361a1c8bc7423483d81",
+    "url": "./static/css/main.17b66461.chunk.css"
+  },
+  {
+    "revision": "d816f8e132c1142755b7",
+    "url": "./static/js/2.94a0b079.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.94a0b079.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f361a1c8bc7423483d81",
+    "url": "./static/js/main.f744341a.chunk.js"
+  },
+  {
+    "revision": "ae35b828c4e77d85f183",
+    "url": "./static/js/runtime-main.3b88d4ec.js"
+  },
+  {
+    "revision": "eb544e55332e9ad6ecfd4f632b9daa86",
+    "url": "./static/media/bg-blue-card-1.eb544e55.svg"
+  },
+  {
+    "revision": "b9b385932911ddc359dbbf105ea82cba",
+    "url": "./static/media/bitcoin.b9b38593.svg"
+  },
+  {
+    "revision": "f98046e96569ed9892a543e1e10692cc",
+    "url": "./static/media/collecting-2.f98046e9.svg"
+  },
+  {
+    "revision": "3d40e5d70da4de77898e91026f462853",
+    "url": "./static/media/collecting.3d40e5d7.svg"
+  },
+  {
+    "revision": "08e6fd25d8eb06af062c8741175f4c64",
+    "url": "./static/media/ethereum.08e6fd25.svg"
+  },
+  {
+    "revision": "07cd6ac8c46dd1b5651c47772d8b392f",
+    "url": "./static/media/graph.07cd6ac8.svg"
+  },
+  {
+    "revision": "4536db170af624ee919de920f607d940",
+    "url": "./static/media/group.4536db17.svg"
+  },
+  {
+    "revision": "64272dde04a5f388f65590367d266bb5",
+    "url": "./static/media/hourglass.64272dde.svg"
+  },
+  {
+    "revision": "a4cb180dfe70f136705e4ebb13453721",
+    "url": "./static/media/ic-dash.a4cb180d.svg"
+  },
+  {
+    "revision": "5853d3625c9e36d5aec33d8eee71b8c1",
+    "url": "./static/media/ic-profile.5853d362.svg"
+  },
+  {
+    "revision": "f024a283248816c1e583c37ffdfd4e10",
+    "url": "./static/media/piggy_bank.f024a283.svg"
+  },
+  {
+    "revision": "c6a0effbe867db0941bd5592f8f43810",
+    "url": "./static/media/search-solid.c6a0effb.svg"
+  }
+]);
